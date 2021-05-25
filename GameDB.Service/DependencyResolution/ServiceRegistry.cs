8b9c883bdedcd1e252dc73a811146deb6ee8d44f
Niputi <7137178+Niputi@users.noreply.github.com>
2021-05-25 09:55:05 +0200

    asp core working auth

--- GameDB.Service/DependencyResolution/ServiceRegistry.cs
+++ GameDB.Service/DependencyResolution/ServiceRegistry.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GameDB.Domain.DomainClasses;
+using GameDB.Service.Middleware;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,6 +16,8 @@ namespace GameDB.Service.DependencyResolution
         {
             AspServiceRegistrations(services);
             services.AddHttpContextAccessor();
+
+            AuthenticationMiddleware.AddAuthentication(services, appSettings);
 
             services.AddScoped<IGameDbApiManager, GameDbApiManager>();
         }
